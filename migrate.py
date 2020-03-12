9dfe655bbcadb7c276aa17a153e911fdc3287bbe
Victor <virb3e@gmail.com>
2020-03-12 17:59:29 +0000

    Fix hardcoded username

--- migrate.py
+++ migrate.py
@@ -6,11 +6,11 @@ config = toml.load("config.toml")
 gitea_api_token = config["gitea"]["api_token"]
 gitlab_api_token = config["gitlab"]["api_token"]
 
-gitea_migrate_url = config["gitea"]["base_url"] + "/api/v1/repos/migrate"
-gitlab_projects_url = config["gitlab"]["base_url"] + "/api/v4/users/virb3/projects"
-
 gitlab_username = config["gitlab"]["username"]
 gitlab_password = config["gitlab"]["password"]
+
+gitea_migrate_url = config["gitea"]["base_url"] + "/api/v1/repos/migrate"
+gitlab_projects_url = config["gitlab"]["base_url"] + f"/api/v4/users/{gitlab_username}/projects"
 
 
 def gitlab_request(url):
